6d9dd674161156100b1f6971686b42ae23ebd789
Rikaty <24g_rko@liceum.ztu.edu.ua>
2025-03-20 09:47:58 +0200

    the new start

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//let user = {};
+    //user.name: "Niko",
+    //console.log(user);
+    //user.age: 30;
+    //console.log(user);
+
+//const message = "JavaScript is awesome!";
+//const isConfirm = confirm(" Please confirm reservation")
+//c
+// const userName = prompt('Please enter your name:');
+// console.log(userName);onsole.log(isConfirm);
+const userName = prompt("Please enter your name:");
+console.log(userName);
+
+const number = prompt("Please enter a number:");
+console.log(number);
